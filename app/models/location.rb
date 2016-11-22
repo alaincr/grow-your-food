b9a64d79926795ca11abee24d356dd4bbd94a13f
alain crawford <alain.crawford@gmail.com>
2016-11-22 18:25:10 +0100

    avancement de la show...

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,10 +1,9 @@
 class Location < ApplicationRecord
   # LOCATION = ["S'initier à l'agriculture urbaine", "Découvrir les bonnes pratiques à la campagne"]
-  belongs_to :user
+  #belongs_to :user
   has_many :bookings
   validates :city, presence: true
-  validates :urban, presence: true
-  # validates :urban, presence: true , inclusion: { in: LOCATION }
+  # validates :urban, presence: true
   validates :capacity, presence: true
 end
 
