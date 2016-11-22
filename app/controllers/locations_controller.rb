6b759c04efa3ee7b9c74caa5a82fb58aa4b1facb
Carole Piette <carole@conscients.com>
2016-11-22 11:39:11 +0100

    conroller index and colum city added to table locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,13 +1,21 @@
 class LocationsController < ApplicationController
 
   def index
+      if params[:city]
+      @locations = Location.where(city: params[:city], urban: params[:urban], capacity: params[:capacity])
+      else
+      @locations = Location.where(urban: params[:urban], capacity: params[:capacity])
+    end
+    #critere date deuxieme temps
+    #critere nbre de personne inf ou eg a nbre de place dispo deuxieme temps
   end
 
   def show
-  end 
+
+  end
 
   def new
-  end 
+  end
 
   def create
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121170516) do
+ActiveRecord::Schema.define(version: 20161122103821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20161121170516) do
     t.datetime "updated_at",   null: false
     t.integer  "booking_id"
     t.integer  "user_id"
+    t.string   "city"
     t.index ["booking_id"], name: "index_locations_on_booking_id", using: :btree
     t.index ["user_id"], name: "index_locations_on_user_id", using: :btree
   end
