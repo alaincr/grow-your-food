046274d27132be60c5521c11ad264e780833a06c
Carole Piette <carole@conscients.com>
2016-11-22 15:28:42 +0100

    home view

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,11 @@
 class Location < ApplicationRecord
+  LOCATION = ["S'initier à l'agriculture urbaine", "Découvrir les bonnes pratiques à la campagne"]
   belongs_to :user
   belongs_to :booking
   validates :city, presence: true
-  validates :urban, presence: true
+  validates :urban, presence: true, inclusion: { in: LOCATION }
   validates :capacity, presence: true
 end
 
 #add dates after
+
