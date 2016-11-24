8a8b04de9e897a00e1a5f0765a7b195b044cff6e
alain crawford <alain.crawford@gmail.com>
2016-11-24 09:44:42 +0100

    en cours

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,10 +8,16 @@ class LocationsController < ApplicationController
     end
     #critere date deuxieme temps
     #critere nbre de personne inf ou eg a nbre de place dispo deuxieme temps
+    @hash = Gmaps4rails.build_markers(@locations) do |flat, marker|
+      marker.lat flat.latitude
+      marker.lng flat.longitude
+    end
   end
 
   def show
     @location = Location.find(params[:id])
+    @hash = [{"lat": @location.latitude, "long": @location.longitude}]
+      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
     #@Booking = Booking.new
   end
 
